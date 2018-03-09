c39f03c164d16164d958885a6c4577d5c5db125e
sandugabriel <sandugabriel@users.noreply.github.com>
2018-03-09 20:58:12 +0200

    Initial commit

---
+++ classes/game.py
@@ -0,0 +1,80 @@
+import random
+from .magic import Spell
+
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class Person:
+    def __init__(self, hp, mp, atk, df, magic, items):
+        self.max_hp = hp
+        self.hp = hp
+        self.max_mp = mp
+        self.mp = mp
+        self.atkl = atk - 10
+        self.atkh = atk + 10
+        self.df = df
+        self.magic = magic
+        self.items = items
+        self.actions = ["Attack", "Magic", "Items"]
+
+    def generate_damage(self):
+        return random.randrange(self.atkl, self.atkh)
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp<0:
+            self.hp = 0
+        return self.hp
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.max_hp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.max_mp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
+    def heal(self, dmg):
+        self.hp += dmg
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
+
+    def choose_action(self):
+        i = 1
+        print("ACTIONS")
+        for item in self.actions:
+            print(str(i) + ":", item)
+            i+=1
+
+    def choose_magic(self):
+        i = 1
+        print("MAGIC")
+        for spell in self.magic:
+            print(str(i) + ":", spell.name, "(cost:", str(spell.cost) + ")")
+            i += 1
+
+    def choose_item(self):
+        i = 1
+        print(bcolors.OKGREEN + bcolors.BOLD + "ITEMS:" + bcolors.ENDC)
+        for item in self.items:
+            print(str(i) + ".", item["item"].name, ":", item["item"].description, " (x" + str(item["quantity"]) + ")")
+            i+=1
+
+
